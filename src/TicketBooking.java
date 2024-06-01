cd8df2248daf8dbb75b8dca7cddd830b6339fe8b
Md. Sadman Mubasshir Khan <sadmankhan@iut-dhaka.edu>
2024-06-01 23:52:59 +0600

    Redesign of booking  and added user home page

--- src/TicketBooking.java
+++ src/TicketBooking.java
@@ -13,7 +13,7 @@ public class TicketBooking extends JFrame{
         setLayout(new BorderLayout());
 
         JPanel titlePanel = new JPanel();
-        ImageIcon AirlineImage = new ImageIcon("FlightImage.png");
+        ImageIcon AirlineImage = new ImageIcon("src\\FlightImage.png");
         JLabel titleImage = new JLabel(AirlineImage, SwingConstants.CENTER);
         titlePanel.add(titleImage);
         titlePanel.setBackground(new Color(0, 0, 0));
